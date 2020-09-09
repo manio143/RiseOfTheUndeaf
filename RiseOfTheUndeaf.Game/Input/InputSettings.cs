915b88260777a3d7daaa7fd387ea44b085edf9f7
Marian Dziubiak <marian.dziubiak@gmail.com>
2020-09-09 12:49:13 +0200

    Move input settings to a separate class and use VirtualButton bindings

---
+++ RiseOfTheUndeaf.Game/Input/InputSettings.cs
@@ -0,0 +1,75 @@
+﻿using Stride.Core;
+using Stride.Input;
+
+namespace RiseOfTheUndeaf.Input
+{
+    [DataContract]
+    public class InputSettings
+    {
+        /// <summary>
+        /// Minimum distance the joystick has to be moved for it to be registered.
+        /// </summary>
+        public float JoystickDeadZone { get; set; } = 0.25f;
+
+        /// <summary>
+        /// Multiplies mouse movement by this amount to apply aim rotations.
+        /// </summary>
+        public float MouseSensitivity { get; set; } = 100.0f;
+
+        /// <summary>
+        /// Bindigs of <see cref="GameButton"/>s to keyboard and gamepad.
+        /// </summary>
+        public VirtualButtonConfig ButtonBindings { get; set; }
+
+        /// <summary>
+        /// A set of default bindings for testing.
+        /// </summary>
+        public static VirtualButtonConfig DefaultButtonBindings()
+        {
+            return new VirtualButtonConfig
+            {
+                // Horizontal movement X axis
+                new VirtualButtonBinding(GameButton.MovementHorizontal,
+                    new VirtualButtonTwoWay(VirtualButton.Keyboard.A, VirtualButton.Keyboard.D)),
+                new VirtualButtonBinding(GameButton.MovementHorizontal,
+                    new VirtualButtonTwoWay(VirtualButton.Keyboard.Left, VirtualButton.Keyboard.Right)),
+                new VirtualButtonBinding(GameButton.MovementHorizontal, VirtualButton.GamePad.LeftThumbAxisX),
+                
+                // Vertical movement Y axis
+                new VirtualButtonBinding(GameButton.MovementVertical,
+                    new VirtualButtonTwoWay(VirtualButton.Keyboard.S, VirtualButton.Keyboard.W)),
+                new VirtualButtonBinding(GameButton.MovementVertical,
+                    new VirtualButtonTwoWay(VirtualButton.Keyboard.Down, VirtualButton.Keyboard.Up)),
+                new VirtualButtonBinding(GameButton.MovementVertical, VirtualButton.GamePad.LeftThumbAxisY),
+
+                // Jumping
+                new VirtualButtonBinding(GameButton.Jump, VirtualButton.Keyboard.Space),
+                new VirtualButtonBinding(GameButton.Jump, VirtualButton.GamePad.A),
+
+                // Horizontal camera X axis
+                new VirtualButtonBinding(GameButton.CameraHorizontal, VirtualButton.Mouse.DeltaX),
+                new VirtualButtonBinding(GameButton.CameraHorizontal, VirtualButton.GamePad.RightThumbAxisX),
+
+                // Vertical camera Y axis
+                new VirtualButtonBinding(GameButton.CameraVertical, VirtualButton.Mouse.DeltaY),
+                new VirtualButtonBinding(GameButton.CameraHorizontal, VirtualButton.GamePad.RightThumbAxisY),
+            };
+        }
+    }
+
+    /// <summary>
+    /// Virtual button definitions for the game.
+    /// </summary>
+    public enum GameButton
+    {
+        Undefined = 0,
+
+        MovementHorizontal,
+        MovementVertical,
+
+        Jump,
+
+        CameraHorizontal,
+        CameraVertical,
+    }
+}
--- RiseOfTheUndeaf.Game/Player/PlayerInput.cs
+++ RiseOfTheUndeaf.Game/Player/PlayerInput.cs
@@ -7,6 +7,7 @@ using Stride.Engine;
 using Stride.Engine.Events;
 using Stride.Input;
 using RiseOfTheUndeaf.Core;
+using RiseOfTheUndeaf.Input;
 
 namespace RiseOfTheUndeaf.Player
 {
@@ -23,41 +24,30 @@ namespace RiseOfTheUndeaf.Player
         public static readonly EventKey<bool> JumpEventKey = new EventKey<bool>();
         private bool jumpButtonDown = false;
 
-        public float DeadZone { get; set; } = 0.25f;
 
         public CameraComponent Camera { get; set; }
 
-        /// <summary>
-        /// Multiplies move movement by this amount to apply aim rotations
-        /// </summary>
-        public float MouseSensitivity = 100.0f;
+        public InputSettings InputSettings { get; set; }
 
-        public List<Keys> KeysLeft { get; } = new List<Keys>();
+        public override void Start()
+        {
+            //TODO: [Stride] bindings serialization
+            if (InputSettings.ButtonBindings == null)
+                InputSettings.ButtonBindings = InputSettings.DefaultButtonBindings();
 
-        public List<Keys> KeysRight { get; } = new List<Keys>();
-
-        public List<Keys> KeysUp { get; } = new List<Keys>();
-
-        public List<Keys> KeysDown { get; } = new List<Keys>();
-
-        public List<Keys> KeysJump { get; } = new List<Keys>();
+            Input.VirtualButtonConfigSet = Input.VirtualButtonConfigSet ?? new VirtualButtonConfigSet();
+            Input.VirtualButtonConfigSet.Add(InputSettings.ButtonBindings);
+        }
 
         public override void Update()
         {
             // Character movement: should be camera-aware
             {
-                // Left stick: movement
-                var moveDirection = Input.GetLeftThumbAny(DeadZone);
-
-                // Keyboard: movement
-                if (KeysLeft.Any(key => Input.IsKeyDown(key)))
-                    moveDirection += -Vector2.UnitX;
-                if (KeysRight.Any(key => Input.IsKeyDown(key)))
-                    moveDirection += +Vector2.UnitX;
-                if (KeysUp.Any(key => Input.IsKeyDown(key)))
-                    moveDirection += +Vector2.UnitY;
-                if (KeysDown.Any(key => Input.IsKeyDown(key)))
-                    moveDirection += -Vector2.UnitY;
+                var moveDirection = new Vector2
+                {
+                    X = Input.GetVirtualButton(0, GameButton.MovementHorizontal),
+                    Y = Input.GetVirtualButton(0, GameButton.MovementVertical),
+                };
 
                 // Broadcast the movement vector as a world-space Vector3 to allow characters to be controlled
                 var worldSpeed = (Camera != null)
@@ -66,7 +56,7 @@ namespace RiseOfTheUndeaf.Player
 
                 // Adjust vector's magnitute - worldSpeed has been normalized
                 var moveLength = moveDirection.Length();
-                var isDeadZoneLeft = moveLength < DeadZone;
+                var isDeadZoneLeft = moveLength < InputSettings.JoystickDeadZone;
                 if (isDeadZoneLeft)
                 {
                     worldSpeed = Vector3.Zero;
@@ -79,7 +69,7 @@ namespace RiseOfTheUndeaf.Player
                     }
                     else
                     {
-                        moveLength = (moveLength - DeadZone) / (1f - DeadZone);
+                        moveLength = (moveLength - InputSettings.JoystickDeadZone) / (1f - InputSettings.JoystickDeadZone);
                     }
 
                     worldSpeed *= moveLength;
@@ -90,9 +80,11 @@ namespace RiseOfTheUndeaf.Player
 
             // Camera rotation: left-right rotates the camera horizontally while up-down controls its altitude
             {
+                //TODO: use virtual buttons data
+
                 // Right stick: camera rotation
-                var cameraDirection = Input.GetRightThumbAny(DeadZone);
-                var isDeadZoneRight = cameraDirection.Length() < DeadZone;
+                var cameraDirection = Input.GetRightThumbAny(InputSettings.JoystickDeadZone);
+                var isDeadZoneRight = cameraDirection.Length() < InputSettings.JoystickDeadZone;
                 if (isDeadZoneRight)
                     cameraDirection = Vector2.Zero;
                 else
@@ -111,7 +103,7 @@ namespace RiseOfTheUndeaf.Player
                 }
                 if (Input.IsMousePositionLocked)
                 {
-                    cameraDirection += new Vector2(Input.MouseDelta.X, -Input.MouseDelta.Y)*MouseSensitivity;
+                    cameraDirection += new Vector2(Input.MouseDelta.X, -Input.MouseDelta.Y) * InputSettings.MouseSensitivity;
                 }
 
                 // Broadcast the camera direction directly, as a screen-space Vector2
@@ -120,13 +112,9 @@ namespace RiseOfTheUndeaf.Player
 
             // Jumping: don't bother with jump restrictions here, just pass the button states
             {
-                // Controller: jumping
-                var isJumpDown = Input.IsGamePadButtonDownAny(GamePadButton.A);
-                var didJump = (!jumpButtonDown && isJumpDown);
+                var isJumpDown = Input.GetVirtualButton(0, GameButton.Jump) > 0;
+                var didJump = (!jumpButtonDown && isJumpDown); //don't signal another jump when button wasn't released
                 jumpButtonDown = isJumpDown;
-
-                // Keyboard: jumping
-                didJump |= (KeysJump.Any(key => Input.IsKeyPressed(key)));
 
                 JumpEventKey.Broadcast(didJump);
             }
