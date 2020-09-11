2c6e5250c17dc72810c69f23026d4d444c47f76c
Marian Dziubiak <marian.dziubiak@gmail.com>
2020-09-11 16:48:01 +0200

    Create CharacterMovement (substitues PlayerController)

---
+++ RiseOfTheUndeaf.Game/Character/CharacterMovementComponent.cs
@@ -0,0 +1,28 @@
+﻿using Stride.Core;
+using Stride.Core.Mathematics;
+using Stride.Engine;
+using Stride.Engine.Design;
+using System.ComponentModel;
+
+namespace RiseOfTheUndeaf.Character
+{
+    [DataContract]
+    [DefaultEntityComponentProcessor(typeof(CharacterMovementProcessor))]
+    public class CharacterMovementComponent : EntityComponent
+    {
+        [DefaultValue(10f)]
+        public float MovementSpeed { get; set; }
+
+        [DataMemberIgnore]
+        public Vector3 LastMoveDirection { get; set; } = Vector3.Zero;
+
+        internal Vector3? MoveDirection { get; set; }
+        public void Move(Vector3 moveDirection) => MoveDirection = moveDirection;
+
+        /// <summary>
+        /// If true the character tries to jump.
+        /// </summary>
+        internal bool ShouldJump { get; set; }
+        public void Jump() => ShouldJump = true;
+    }
+}
---
+++ RiseOfTheUndeaf.Game/Character/CharacterMovementProcessor.cs
@@ -0,0 +1,81 @@
+﻿using Stride.Core.Annotations;
+using Stride.Core.Mathematics;
+using Stride.Engine;
+using Stride.Games;
+using Stride.Physics;
+using System;
+using System.Linq;
+
+namespace RiseOfTheUndeaf.Character
+{
+    public class CharacterMovementProcessor : EntityProcessor<CharacterMovementComponent, CharacterMovementProcessor.Data>
+    {
+        public class Data
+        {
+            public CharacterComponent Character { get; set; }
+            public TransformComponent ModelEntityTransform { get; set; }
+            public float YawOrientation { get; set; }
+        }
+
+        public CharacterMovementProcessor() : base(typeof(CharacterComponent))
+        { }
+
+        protected override Data GenerateComponentData([NotNull] Entity entity, [NotNull] CharacterMovementComponent component)
+        {
+            return new Data
+            {
+                Character = entity.Get<CharacterComponent>(),
+                ModelEntityTransform = entity.GetChildren()
+                    .First(e => e.Name == "CharacterModel").Get<TransformComponent>(),
+            };
+        }
+
+        public override void Update(GameTime time)
+        {
+            foreach (var kvp in ComponentDatas)
+            {
+                var movementComponent = kvp.Key;
+                var data = kvp.Value;
+
+                if(movementComponent.MoveDirection.HasValue)
+                {
+                    Move(movementComponent, data, movementComponent.MoveDirection.Value);
+                    movementComponent.MoveDirection = null;
+                }
+                if(movementComponent.ShouldJump)
+                {
+                    Jump(movementComponent, data);
+                    movementComponent.ShouldJump = false;
+                }
+            }
+        }
+
+        private void Jump(CharacterMovementComponent movementComponent, Data data)
+        {
+            if (data.Character.IsGrounded)
+            {
+                data.Character.Jump();
+                ; // TODO: broadcast groundness to children (animation)
+            }
+            else
+                ; // TODO: broadcast groundness to children (animation)
+        }
+
+        private void Move(CharacterMovementComponent component, Data data, Vector3 moveDirection)
+        {
+            // Allow very simple inertia to the character to make animation transitions more fluid
+            component.LastMoveDirection = component.LastMoveDirection * 0.85f + moveDirection * 0.15f;
+
+            data.Character.SetVelocity(component.LastMoveDirection * component.MovementSpeed);
+
+            // TODO: broadcast component.LastMoveDirection.Length() to children as speed (animation)
+
+            // Character orientation
+            if (moveDirection.Length() > 0.001)
+            {
+                data.YawOrientation = (float)Math.Atan2(-moveDirection.Z, moveDirection.X) + MathUtil.PiOverTwo;
+            }
+            data.ModelEntityTransform.Rotation = Quaternion.RotationYawPitchRoll(data.YawOrientation, 0, 0);
+        }
+    }
+}
