3cf2cf661645f08246a56c785e158f09abff3029
Marian Dziubiak <marian.dziubiak@gmail.com>
2020-09-14 08:12:50 +0200

    Modify dynamically emitted method, not to alocate

--- RiseOfTheUndeaf.Game/EntityEvents/EntityEventExtension.cs
+++ RiseOfTheUndeaf.Game/EntityEvents/EntityEventExtension.cs
@@ -8,9 +8,44 @@ using System.Collections;
 
 namespace RiseOfTheUndeaf.EntityEvents
 {
+    // This class provides a mechanism for broadcasting events throughout the entity tree.
+    // Given an event interface:
+    //
+    //    public interface IMyEvent : IEntityEvent
+    //    {
+    //        void Event(int value);
+    //    }
+    //
+    // Calling `Entity.BroadcastEvent<IMyEvent>()` will return a new object, implementing
+    // the `IMyEvent` interface, called a broadcasting object, whose type is dynamically created.
+    //
+    //    public class DynamicImplementationOfIMyEvent : IMyEvent
+    //    {
+    //        private Entity entity;
+    //        public DynamicImplementationOfIMyEvent(Entity arg1) => entity = arg1;
+    //        public void Event(int value)
+    //        {
+    //            foreach (var component in entity.Components)
+    //                if (component is IMyEvent @event)
+    //                    @event.Event(value);
+    //            var parent = entity;
+    //            foreach(var child in entity.GetChildren())
+    //            {
+    //                entity = child;
+    //                this.Event(value);
+    //            }
+    //            entity = parent;
+    //        }
+    //    }
+
     public static class EntityEventExtension
     {
+        /// <summary>
+        /// Cache of &lt;interface, broadcaster&gt; types, because reflection and emit are slow.
+        /// </summary>
         private static Dictionary<Type, Type> dynamicInstanceCache = new Dictionary<Type, Type>();
+
+        // Only create one assembly with one module
         private static AssemblyBuilder dynamicAssebly;
         private static ModuleBuilder dynamicModule;
 
@@ -20,9 +55,13 @@ namespace RiseOfTheUndeaf.EntityEvents
         /// <typeparam name="TEvent">Event interface type</typeparam>
         /// <param name="entity">Entity that initiates broadcast</param>
         /// <returns>Broadcasting object for given <paramref name="entity"/></returns>
+        /// <remarks>The interface <typeparamref name="TEvent"/> may only carry non-generic, void methods.</remarks>
         public static TEvent BroadcastEvent<TEvent>(this Entity entity) where TEvent : IEntityEvent
         {
             var interfaceType = typeof(TEvent);
+
+            if (!interfaceType.IsInterface)
+                throw new ArgumentException("The generic argument must be an interface.", nameof(TEvent));
 
             // use cache if possible
             if (dynamicInstanceCache.TryGetValue(interfaceType, out var instanceType))
@@ -30,15 +69,27 @@ namespace RiseOfTheUndeaf.EntityEvents
                 return (TEvent)Activator.CreateInstance(instanceType, entity);
             }
 
-            dynamicAssebly ??= AssemblyBuilder.DefineDynamicAssembly(
-                new AssemblyName("DynamicAssembly"),
-                AssemblyBuilderAccess.Run);
-
-            dynamicModule ??= dynamicAssebly.DefineDynamicModule("DynamicModule");
+            EnsureDynamicModule();
 
             var typeName = $"DynamicImplementationOf{interfaceType.Name}";
             var typeBuilder = dynamicModule.DefineType(typeName, TypeAttributes.Public | TypeAttributes.Class);
+            
+            EmitTypeMembers(interfaceType, typeBuilder);
 
+            typeBuilder.AddInterfaceImplementation(interfaceType);
+            var type = typeBuilder.CreateTypeInfo().AsType();
+
+            dynamicInstanceCache.Add(interfaceType, type);
+            return (TEvent)Activator.CreateInstance(type, entity);
+        }
+
+        /// <summary>
+        /// Emits a field of <see cref="Entity"/>, a constructor (which sets the field) and interface methods for broadcasting.
+        /// </summary>
+        /// <param name="interfaceType">Type of interface to be implemented</param>
+        /// <param name="typeBuilder">Type being constructed</param>
+        private static void EmitTypeMembers(Type interfaceType, TypeBuilder typeBuilder)
+        {
             var entityField = typeBuilder.DefineField("entity", typeof(Entity), FieldAttributes.Private);
 
             var ctor = typeBuilder.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard, new Type[] { typeof(Entity) });
@@ -49,14 +100,25 @@ namespace RiseOfTheUndeaf.EntityEvents
             {
                 BuildMethod(typeBuilder, method, entityField, interfaceType);
             }
-
-            typeBuilder.AddInterfaceImplementation(interfaceType);
-            var type = typeBuilder.CreateTypeInfo().AsType();
-
-            dynamicInstanceCache.Add(interfaceType, type);
-            return (TEvent)Activator.CreateInstance(type, entity);
         }
 
+        /// <summary>
+        /// Initializes <see cref="dynamicAssebly"/> and <see cref="dynamicModule"/>.
+        /// </summary>
+        private static void EnsureDynamicModule()
+        {
+            dynamicAssebly ??= AssemblyBuilder.DefineDynamicAssembly(
+                            new AssemblyName("DynamicAssembly"),
+                            AssemblyBuilderAccess.Run);
+
+            dynamicModule ??= dynamicAssebly.DefineDynamicModule("DynamicModule");
+        }
+
+        /// <summary>
+        /// Emits constructor IL that takes one parameter of type <see cref="Entity"/> and saves it in the <paramref name="entityField"/>.
+        /// </summary>
+        /// <param name="entityField"></param>
+        /// <param name="ctor"></param>
         private static void EmitConstructor(FieldBuilder entityField, ConstructorBuilder ctor)
         {
             var ctorIL = ctor.GetILGenerator();
@@ -76,6 +138,13 @@ namespace RiseOfTheUndeaf.EntityEvents
             ctorIL.Emit(OpCodes.Ret);
         }
 
+        /// <summary>
+        /// Creates a broadcasting method, implementation of <paramref name="imethod"/>, on the new type.
+        /// </summary>
+        /// <param name="typeBuilder">Type under construction</param>
+        /// <param name="imethod">Method to be implemented</param>
+        /// <param name="entityField">Private field for the current <see cref="Entity"/></param>
+        /// <param name="interfaceType">Interface to be implemented</param>
         private static void BuildMethod(TypeBuilder typeBuilder, MethodInfo imethod, FieldBuilder entityField, Type interfaceType)
         {
             if (imethod.ReturnType != typeof(void))
@@ -89,92 +158,109 @@ namespace RiseOfTheUndeaf.EntityEvents
             );
             var il = method.GetILGenerator();
 
-            // create a new object x of type * : TEvent
-            // such that x.SomeMethod(params) {
-            //    foreach(var component in entity.Components)
-            //        if(component is TEvent) component.SomeMethod(params)
-            //    foreach(var child in entity.GetChildren)
-            //        child.BroadcastEvent<TEvent>().SomeMethod(params);
-            // }
-
             var componentEnumerator = il.DeclareLocal(typeof(IEnumerator<EntityComponent>));
             var entityEnumerator = il.DeclareLocal(typeof(IEnumerator<Entity>));
+            var parentEntity = il.DeclareLocal(typeof(Entity));
 
-            il.Emit(OpCodes.Ldarg_0);
-            il.Emit(OpCodes.Ldfld, entityField); //stack: entity
-            il.Emit(OpCodes.Dup); //stack: entity, entity
-
-            il.EmitCall(OpCodes.Callvirt, typeof(Entity).GetProperty("Components").GetGetMethod(), null); //stack: entity, components
-            il.EmitCall(OpCodes.Callvirt, typeof(IEnumerable<EntityComponent>).GetMethod("GetEnumerator"), null); //stack: entity, enumerator
-            il.Emit(OpCodes.Stloc, componentEnumerator); //stack: entity
-
-            var loop1start = il.DefineLabel();
-            var loop1end = il.DefineLabel();
-
-            il.MarkLabel(loop1start);
-
-            // if(!componentEnumerator.MoveNext) jump to end
-            il.Emit(OpCodes.Ldloc, componentEnumerator); //stack: entity, enumerator
-            il.Emit(OpCodes.Dup); //stack: entity, enumerator, enumerator
-            il.EmitCall(OpCodes.Callvirt, typeof(IEnumerator).GetMethod("MoveNext"), null); //stack: entity, enumerator, bool
-
-            il.Emit(OpCodes.Brfalse_S, loop1end); //stack: entity, enumerator
-
-            // else{if(enumerator.Current is TEvent) component.SomeMethod(params)
-            il.EmitCall(OpCodes.Callvirt, typeof(IEnumerator).GetProperty("Current").GetGetMethod(), null); //stack: entity, component
-            il.Emit(OpCodes.Isinst, interfaceType); //stack: entity, component/null
-            il.Emit(OpCodes.Dup); //stack: entity, component/null, component/null
-
-            var if1 = il.DefineLabel();
-            var if1_ = il.DefineLabel();
-            il.Emit(OpCodes.Brfalse_S, if1); //stack: entity, component/null
-            for (int i = 1; i <= imethod.GetParameters().Length; i++)
+            // foreach (var component in entity.Components)
+            //    if (component is IInterface @event)
+            //       @event.Event(params...);
             {
-                il.Emit(OpCodes.Ldarg, i);
+                il.Emit(OpCodes.Ldarg_0);
+                il.Emit(OpCodes.Ldfld, entityField); //stack: entity
+                il.Emit(OpCodes.Dup); //stack: entity, entity
+
+                il.EmitCall(OpCodes.Callvirt, typeof(Entity).GetProperty("Components").GetGetMethod(), null); //stack: entity, components
+                il.EmitCall(OpCodes.Callvirt, typeof(IEnumerable<EntityComponent>).GetMethod("GetEnumerator"), null); //stack: entity, enumerator
+                il.Emit(OpCodes.Stloc, componentEnumerator); //stack: entity
+
+                var loop1start = il.DefineLabel();
+                var loop1end = il.DefineLabel();
+
+                il.MarkLabel(loop1start);
+
+                // if(!componentEnumerator.MoveNext) jump to end
+                il.Emit(OpCodes.Ldloc, componentEnumerator); //stack: entity, enumerator
+                il.Emit(OpCodes.Dup); //stack: entity, enumerator, enumerator
+                il.EmitCall(OpCodes.Callvirt, typeof(IEnumerator).GetMethod("MoveNext"), null); //stack: entity, enumerator, bool
+
+                il.Emit(OpCodes.Brfalse_S, loop1end); //stack: entity, enumerator
+
+                // else{if(enumerator.Current is TEvent) component.SomeMethod(params)
+                il.EmitCall(OpCodes.Callvirt, typeof(IEnumerator).GetProperty("Current").GetGetMethod(), null); //stack: entity, component
+                il.Emit(OpCodes.Isinst, interfaceType); //stack: entity, component/null
+                il.Emit(OpCodes.Dup); //stack: entity, component/null, component/null
+
+                var if1 = il.DefineLabel();
+                var if1_ = il.DefineLabel();
+                il.Emit(OpCodes.Brfalse_S, if1); //stack: entity, component/null
+                for (int i = 1; i <= imethod.GetParameters().Length; i++)
+                {
+                    il.Emit(OpCodes.Ldarg, i);
+                }
+                il.EmitCall(OpCodes.Callvirt, imethod, null); //stack: entity
+
+                il.Emit(OpCodes.Br_S, if1_);
+
+                il.MarkLabel(if1);
+                il.Emit(OpCodes.Pop); //stack: entity
+
+                il.MarkLabel(if1_);
+                il.Emit(OpCodes.Br, loop1start);
+
+                il.MarkLabel(loop1end);
+                il.Emit(OpCodes.Pop); //stack: entity
             }
-            il.EmitCall(OpCodes.Callvirt, imethod, null); //stack: entity
 
-            il.Emit(OpCodes.Br_S, if1_);
-
-            il.MarkLabel(if1);
-            il.Emit(OpCodes.Pop); //stack: entity
-
-            il.MarkLabel(if1_);
-            il.Emit(OpCodes.Br, loop1start);
-
-            il.MarkLabel(loop1end);
-            il.Emit(OpCodes.Pop); //stack: entity
-
-            il.EmitCall(OpCodes.Call, typeof(EntityExtensions).GetMethod("GetChildren", BindingFlags.Static | BindingFlags.Public), null); //stack: children
-            il.EmitCall(OpCodes.Callvirt, typeof(IEnumerable<Entity>).GetMethod("GetEnumerator"), null); //stack: enumerator
-            il.Emit(OpCodes.Stloc, entityEnumerator); //stack:
-
-            var loop2start = il.DefineLabel();
-            var loop2end = il.DefineLabel();
-
-            il.MarkLabel(loop2start);
-
-            // if(!entityEnumerator.MoveNext) jump to end
-            il.Emit(OpCodes.Ldloc, entityEnumerator); //stack: enumerator
-            il.Emit(OpCodes.Dup); //stack: enumerator, enumerator
-            il.EmitCall(OpCodes.Callvirt, typeof(IEnumerator).GetMethod("MoveNext"), null); //stack: enumerator, bool
-
-            il.Emit(OpCodes.Brfalse_S, loop2end); //stack: enumerator
-
-            // child.BroadcastEvent<TEvent>().SomeMethod(params);)
-            il.EmitCall(OpCodes.Callvirt, typeof(IEnumerator).GetProperty("Current").GetGetMethod(), null); //stack: entity
-
-            il.EmitCall(OpCodes.Call, typeof(EntityEventExtension).GetMethod(nameof(BroadcastEvent), BindingFlags.Static | BindingFlags.Public).MakeGenericMethod(interfaceType), null); //stack: interface
-            for (int i = 1; i <= imethod.GetParameters().Length; i++)
+            // var parent = entity;
+            // foreach(var child in entity.GetChildren())
+            // {
+            //     entity = child;
+            //     this.Event(params...);
+            // }
+            // entity = parent;
             {
-                il.Emit(OpCodes.Ldarg, i);
+                il.Emit(OpCodes.Dup); //stack: entity, entity
+                il.Emit(OpCodes.Stloc, parentEntity); //stack: entity
+
+                il.EmitCall(OpCodes.Call, typeof(EntityExtensions).GetMethod("GetChildren", BindingFlags.Static | BindingFlags.Public), null); //stack: children
+                il.EmitCall(OpCodes.Callvirt, typeof(IEnumerable<Entity>).GetMethod("GetEnumerator"), null); //stack: enumerator
+                il.Emit(OpCodes.Stloc, entityEnumerator); //stack:
+
+                var loop2start = il.DefineLabel();
+                var loop2end = il.DefineLabel();
+
+                il.MarkLabel(loop2start);
+
+                il.Emit(OpCodes.Ldarg_0); //stack: this
+
+                // if(!entityEnumerator.MoveNext) jump to end
+                il.Emit(OpCodes.Ldloc, entityEnumerator); //stack: this, enumerator
+                il.Emit(OpCodes.Dup); //stack: this, enumerator, enumerator
+                il.EmitCall(OpCodes.Callvirt, typeof(IEnumerator).GetMethod("MoveNext"), null); //stack: this, enumerator, bool
+
+                il.Emit(OpCodes.Brfalse_S, loop2end); //stack: this, enumerator
+
+                il.EmitCall(OpCodes.Callvirt, typeof(IEnumerator).GetProperty("Current").GetGetMethod(), null); //stack: this, entity
+
+                il.Emit(OpCodes.Stfld, entityField); //stack:
+                il.Emit(OpCodes.Ldarg_0); //stack: this
+
+                // call this.Method(params...) with this.enity = child
+                for (int i = 1; i <= imethod.GetParameters().Length; i++)
+                {
+                    il.Emit(OpCodes.Ldarg, i);
+                }
+                il.EmitCall(OpCodes.Callvirt, method, null); //stack:
+
+                il.Emit(OpCodes.Br, loop2start);
+
+                il.MarkLabel(loop2end);
+                il.Emit(OpCodes.Pop); //stack: this
+
+                il.Emit(OpCodes.Ldloc, parentEntity); //stack: this, entity
+                il.Emit(OpCodes.Stfld, entityField); //stack:
             }
-            il.EmitCall(OpCodes.Callvirt, imethod, null); //stack:
-
-            il.Emit(OpCodes.Br, loop2start);
-
-            il.MarkLabel(loop2end);
-            il.Emit(OpCodes.Pop); //stack:
 
             il.Emit(OpCodes.Ret);
         }
